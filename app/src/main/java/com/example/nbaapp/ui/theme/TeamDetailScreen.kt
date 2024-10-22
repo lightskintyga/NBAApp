09f49602c5704171be80dd04990729f945983b33
lightskintyga <daniilivanov7010@gmail.com>
2024-10-22 14:47:47 +0500

    Сделана основная логика

--- app/src/main/java/com/example/nbaapp/ui/theme/TeamDetailScreen.kt
+++ app/src/main/java/com/example/nbaapp/ui/theme/TeamDetailScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
